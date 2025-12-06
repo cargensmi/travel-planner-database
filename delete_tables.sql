e6c1edcf78ae363dadcb9d4898d80f9222c29c84
cargensmi <cargensmi@gmail.com>
2025-12-06 11:53:47 -0500

    Update delete_tables.sql

--- delete_tables.sql
+++ delete_tables.sql
@@ -2,11 +2,11 @@
 DROP VIEW IF EXISTS trip_summary;
 
 -- Drop tables in order to avoid foreign key conflicts
-DROP TABLE IF EXISTS TRIP;
 DROP TABLE IF EXISTS FAVORITE;
 DROP TABLE IF EXISTS EXPENSE;
 DROP TABLE IF EXISTS HOTEL;
 DROP TABLE IF EXISTS ACTIVITY;
+DROP TABLE IF EXISTS TRIP;
 
 -- Optional: confirm deletion by checking tables
 SHOW TABLES;
