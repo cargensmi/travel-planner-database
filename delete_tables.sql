b2cd7144d3ebe1daa3794e3bcae9915e8c481397
cargensmi <cargensmi@gmail.com>
2025-12-06 11:52:14 -0500

    Create delete_tables.sql

---
+++ delete_tables.sql
@@ -0,0 +1,12 @@
+-- Drop the view first (if it exists)
+DROP VIEW IF EXISTS trip_summary;
+
+-- Drop tables in order to avoid foreign key conflicts
+DROP TABLE IF EXISTS FAVORITE;
+DROP TABLE IF EXISTS EXPENSE;
+DROP TABLE IF EXISTS HOTEL;
+DROP TABLE IF EXISTS ACTIVITY;
+DROP TABLE IF EXISTS TRIP;
+
+-- Optional: confirm deletion by checking tables
+SHOW TABLES;
